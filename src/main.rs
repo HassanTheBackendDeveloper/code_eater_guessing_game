56b57795618ad2ce23141df1ffa382d1af4d8340
Hassan Murtaza <hassan.murtaza@smartfunstudios.com>
2024-07-19 12:09:45 +0500

    code eater gussing game

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use std::io;
+use rand::Rng;
+use colored::Colorize;
+
+fn main() {
+    println!("{}", "Guessing Game!".yellow());
+
+    let fruits = [
+        "apple",
+        "banana",
+        "cherry",
+        "date",
+        "elderberry",
+        "fig",
+        "grape",
+        "honeydew",
+        "kiwi",
+        "lemon",
+        "mango",
+        "nectarine",
+        "orange",
+        "papaya",
+        "quince",
+        "raspberry",
+        "strawberry",
+        "tangerine",
+    ];
+
+    for (index, value) in fruits.iter().enumerate() {
+        println!("{}", format!("{}: {}", index, value).purple());
+    }
+
+    let secret_fruit = fruits[rand::thread_rng().gen_range(0..fruits.len())];
+
+    println!("Secret Fruit is: {}", secret_fruit);
+
+    loop {
+        println!("{}", "Please enter your guess fruit.".yellow());
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Failed to read line.");
+
+        let guess = guess.to_lowercase();
+
+        let guess = guess.trim();
+
+        // println!("Your guess is '{}'", guess);
+
+        if guess == secret_fruit {
+            println!("{}", "Correct Guess! You are winner!".green().bold());
+            break;
+        } else {
+            println!("{}", "Your guess is wrong!".red().bold());
+        }
+    }
+}
